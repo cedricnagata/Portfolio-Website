4749e55f92b3f06ee63c25878e4043114475ccf9
cedricnagata <cedric.nagata@gmail.com>
2023-10-17 13:01:15 -0700

    Fixed url

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # or your frontend’s deployed domain
     "https://portfolio-website-iota-teal.vercel.app"
+    "https://cedricnagata.com"
 ]
 
 ROOT_URLCONF = 'myportfolio.urls'
