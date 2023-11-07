a9d966f7a11b6550d8851da7933aa476b1abc740
cedricnagata <cedric.nagata@gmail.com>
2023-11-07 15:53:02 -0800

    Added "made with" to home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ const Home = () => (
     <div className="home-content">
         <h1>Hello! My Name is Cedric.</h1>
         <p>Welcome to My Portfolio</p>
+        <p>(Made with Django and React)</p>
     </div>
 );
 
